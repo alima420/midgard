2b25a2274cc3283f5bdf1a778222a446932f7557
alima420 <165353508+alima420@users.noreply.github.com>
2024-08-06 05:06:10 +0200

    rule violation update

--- machine-frontend/pages/Inverter.vue
+++ machine-frontend/pages/Inverter.vue
@@ -60,9 +60,9 @@
         const url = 'https://fab.solarnative.cloud/v1/debug/inverter/data';
         const region = 'eu-central-1';
         const service = 'execute-api';
-        const awsAccessKeyId = 'ASIATFDF4VDRN64NV52B';
-        const awsSecretAccessKey = '6GWsjSWyl9ZwzpjZ1Gwrj73eHE5mbtoFKm5glJ1i';
-        const awsSessionToken = 'IQoJb3JpZ2luX2VjENr//////////wEaDGV1LWNlbnRyYWwtMSJIMEYCIQDF7a1Rq7v9mEqX9ekDA45g3LCGE5mfQk30AaoDPZAASAIhAMuYEoIkU1O7DzcuSvladmW2bVPkNeQwZRCZjx7olqSkKt8ECMP//////////wEQABoMMjE3MTA5NTM5MDQyIgwHfJY8t+s66XjL4K0qswSCjr+cd/wLU9Devk0t4D9ExCfSTPF6PerTWnMhgnBsAe0vHgoEolqg7e+NGU9avZKMDW2gyaaXwm9LSHi/3wVH3OVl0SHQKqS3QFmLnDxRPaBvE5obS0y+gei5CvjslY6T2IWPQLiOmNKoswik6nh79WpJxKlxlO39L0OoVN2o2PmvxWWsHPwQHVeFH/AF00H+rbI8hW4SbNVN1CrRbOPmEnOh89mZbaGaOEj/en7lOKAC8hj5jntQBx3gZq94qCJgbo5kko3EmEf3jwsF5w7l9As4qZNzrbTzhi34SdsjWJswraXWK5Z8Sh04I29JSd0mpZhkl8HX9Og0gHFNkLMAGyXTVAXJrM0VwbiX95izWmXgGyFkcWTJSrsZQ86z3VQQRu4gAXXso9/4wrG4TQmKEoUQgZp7XVlQkhREVgYRmZo7QzkDtbxS59Vq2pWh2WydWs5M5s5sunaeoDrw1Gpmt/UABj7JbCHDrc/9kPkgLZoWn1v+MRpEazXAIzHcIGIFvdmEQUc3qsUPMfZa15Q2cKetRVyxgYk0wG4ZIuGquTbgc/yhiXpYBN1+Vqx0bH9IGQpcUk08beYhs/YgT7MRLbODq2YlA08EPczLE4lcQ0IfuNgZZ73VpayOLzwxG++itb45VzOzThnbnEg/v8pHt9xE2UTNYduZfIH1bOEWzEIQZ0fkG3RbjglP+8aTbeGjogoLEr9epBWl593O9IgtbaoA/ivFx6GZyyctsMXatnORJjC4jL+1BjqEAjbwaQmQKdmaDHcLpvvKo60laE2osCtP38ZANM9eVMNn37NAXOklA7Vb9UoN1fTo1363rXjcEBN5ZwzGiqz/VhPk8ohwIwth58rK7KYmLZJixAX+Ub4mffnj98RZVb+pDo1FDnQjGbYCemlxKO8F9HsYCc/7+PU9C+CX2eDTC1j+0+NSe5RjP/1NXwxS5hL397T8f/R5hfp+7/3xLTyAoRBEDx/wGqiSYYlyw2Ch9NKmJTD0OEpgNCsHMfs5Oiwe/17XMj5hBZ9T/8oo+MqwWbFK5FHhdwcACxVJ6RlYyymJpeDRY2C/8ftREXlNuUsI3DRRjB5E31P9FTp003gS+fUJF1lg';
+        const awsAccessKeyId = 'awsAccessKeyId';
+        const awsSecretAccessKey = 'awsSecretAccessKey';
+        const awsSessionToken = 'awsSessionToken';
         const credentials = new AWS.Credentials(awsAccessKeyId, awsSecretAccessKey, awsSessionToken);
         AWS.config.update({ region, credentials });
   
